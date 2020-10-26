a7b57f4173973088d0dcc5f5097915eb688eabb0
IoannaMi <ioannamicha@hotmail.com>
2020-10-26 20:34:44 +0100

    add request service in the menu-components

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -2,14 +2,21 @@
   <div id="menu">
     <v-layout column fill-height justfy-space-around class="pa-4">
       <v-flex grow>
-        <data-layers
+<!--         <data-layers
           :layers.sync="layers"
           :map="map"
           @data-layers="$emit('update:data-layers', dataLayers)"
-        />
+        /> -->
+        <downscaled-layers
+          :visibility="visibility"
+        >
+        </downscaled-layers>
       </v-flex>
       <v-flex grow>
         <import-layers :map="map" />
+      </v-flex>
+      <v-flex grow>
+        <request-service></request-service>
       </v-flex>
       <v-flex grow>
         <export-layers :map="map" />
@@ -24,53 +31,57 @@
 </template>
 
 <script>
-import DataLayers from './DataLayers'
-import exportLayers from './ExportLayers'
-import ImportLayers from './ImportLayers'
-import exportDialog from './ExportDialog'
-import plotDialog from './PlotDialog'
-import FileReader from 'filereader'
+import exportLayers from "./ExportLayers";
+import ImportLayers from "./ImportLayers";
+/* import exportDialog from "./ExportDialog";
+import plotDialog from "./PlotDialog"; */
+import RequestService from "./RequestService";
+import DownscaledLayers from "./DownscaledLayers";
 
 export default {
-  name: 'map-component',
+  name: "map-component",
   props: {
     map: {
       type: Object
     },
     layers: {
       type: Array
+    },
+    visibility: { 
+      type: Boolean
     }
   },
   watch: {
     map: function(map) {
-      this.map = map
+      this.map = map;
     },
     layers: function(layers) {
-      console.log('layers', layers)
-      this.layers = layers
+      /* console.log("layers", layers); */
+      this.layers = layers;
     }
   },
   mounted() {
-    console.log(this.layers)
+    /* console.log(this.layers); */
   },
   data: () => {
     return {
       exportDialog: false,
       plotDialog: false
-    }
+    };
   },
   components: {
-    DataLayers,
-    exportDialog,
-    plotDialog,
+    DownscaledLayers,
+    /* exportDialog, */
+    /* plotDialog, */
     exportLayers,
-    ImportLayers
+    ImportLayers,
+    RequestService,
   }
-}
+};
 </script>
 
 <style>
-@import '~mapbox-gl/dist/mapbox-gl.css';
+@import "~mapbox-gl/dist/mapbox-gl.css";
 
 #menu {
   height: 100%;
