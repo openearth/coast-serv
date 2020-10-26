cb58973002ae0af4b8820b5ba7fe2d29fbe0449a
IoannaMi <ioannamicha@hotmail.com>
2020-10-26 20:29:37 +0100

    add html and markdown loader in the config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'coast-serv' : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "coast-serv" : "/",
+  chainWebpack: config => {
+    config.module
+      .rule("markdown")
+      .test(/\.md$/)
+      .use("html-loader")
+      .loader("html-loader")
+      .end()
+      .use("markdown-loader")
+      .loader("markdown-loader")
+      .end();
+  }
+};
