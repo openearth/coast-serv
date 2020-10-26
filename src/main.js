deb5de7c7237ac480dc4dff2a70649fb3fef3930
IoannaMi <ioannamicha@hotmail.com>
2020-10-26 12:52:49 +0100

    import store

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import vuetify from './plugins/vuetify'
-import App from './App.vue'
-import Vue2MapboxGL from 'vue2mapbox-gl'
-import { config } from './config/framework-config.js'
+import Vue from "vue";
+import vuetify from "./plugins/vuetify";
+import App from "./App.vue";
+import Vue2MapboxGL from "vue2mapbox-gl";
+import { config } from "./config/framework-config.js";
+import store from "./store";
 
-Vue.prototype.appConfig = config
-Vue.use(Vue2MapboxGL)
-Vue.config.productionTip = false
+Vue.prototype.appConfig = config;
+Vue.use(Vue2MapboxGL);
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
